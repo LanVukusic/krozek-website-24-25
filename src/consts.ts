61b75c94a807c2ef721ef7a7548d4a05db94339f
Lan Vukušič <lan.vukusic@gmail.com>
2023-11-11 10:08:16 +0100

    prva verzija

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Krožek 2023/24";
+export const SITE_DESCRIPTION = "Računalniški krožek 2023 / 24 - Python";
